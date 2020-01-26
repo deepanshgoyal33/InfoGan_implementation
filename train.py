08623e2892cd3dfa236d86a21d98e44624946d9c
Deepansh Goyal <deepanshgoyal33@gmail.com>
2020-01-26 19:05:50 +0530

    model_setup

---
+++ train.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+import random
+import model
